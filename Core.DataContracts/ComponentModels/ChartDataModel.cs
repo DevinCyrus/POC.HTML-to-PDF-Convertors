f2d1816f314b445d4b25207a939f04af7e4506c2
DevinCyrus <Devin.Cyrus@1Nebula.com>
2025-09-11 15:59:54 +0200

    Initial testing of Razor Components

---
+++ Core.DataContracts/ComponentModels/ChartDataModel.cs
@@ -0,0 +1,7 @@
+﻿namespace DataContracts.ComponentModels;
+
+public class ChartDataModel
+{
+	public List<string> Labels { get; set; } = new List<string>();
+	public List<decimal> Values { get; set; } = new List<decimal>();
+}
---
+++ Core.DataContracts/ComponentModels/LineChartModel.cs
@@ -0,0 +1,7 @@
+﻿namespace DataContracts.ComponentModels;
+
+public class LineChartModel
+{
+	public string Title { get; set; } = "Monthly Cloud Spend Trend";
+	public ChartDataModel Data { get; set; } = new ChartDataModel();
+}
